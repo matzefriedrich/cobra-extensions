90d75eca3b7a183fe1639538372b2fd1d13dbbe7
Matthias Friedrich <1573457+matzefriedrich@users.noreply.github.com>
2024-09-25 22:28:23 +0200

    Adds comments to ExecutableCommand interface and BaseCommand

--- pkg/abstractions/base_command.go
+++ pkg/abstractions/base_command.go
@@ -1,11 +1,15 @@
 package abstractions
 
+// ExecutableCommand defines an interface for commands that can be executed.
 type ExecutableCommand interface {
+
+	// Execute runs the command, performing its specific action based on the implementation.
 	Execute()
 }
 
 // BaseCommand A base type for typed commands.
 type BaseCommand struct{}
 
+// Execute performs the primary action defined by the BaseCommand.
 func (c *BaseCommand) Execute() {
 }
