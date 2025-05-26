f179780358645cadfc3e4eee3f872b5c51f128af
Matthias Friedrich <1573457+matzefriedrich@users.noreply.github.com>
2025-05-26 22:02:07 +0200

    Removes example command implementations to thin-out package dependencies

--- example/commands/crypt.go
+++
@@ -1,6 +0,0 @@
-package commands
-
-// cryptCommand A base command type for crypt commands.
-type cryptCommand struct {
-	Passphrase string `flag:"passphrase" usage:"The passphrase protecting a message."`
-}
